8a19621551b2f6f2cf0a874fbde782c2a160910b
PestArzt <kirilll.baranov@mail.ru>
2024-01-08 19:49:54 +0300

    Create a disk workbench(doesn't work yet)

--- src/main/java/net/pestarzt/pestarzt_discs/PestArzt_Discs.java
+++ src/main/java/net/pestarzt/pestarzt_discs/PestArzt_Discs.java
@@ -9,6 +9,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.pestarzt.pestarzt_discs.block.ModBlocks;
 import net.pestarzt.pestarzt_discs.item.ModItems;
 import org.slf4j.Logger;
 
@@ -23,6 +24,7 @@ public class PestArzt_Discs
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
