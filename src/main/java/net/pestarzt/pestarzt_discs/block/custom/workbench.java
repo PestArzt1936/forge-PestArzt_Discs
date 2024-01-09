033ca9a21a45d202d73147f424b4a0486f440140
PestArzt <kirilll.baranov@mail.ru>
2024-01-09 21:52:08 +0300

    Fixed textures and names of items

---
+++ src/main/java/net/pestarzt/pestarzt_discs/block/custom/workbench.java
@@ -0,0 +1,10 @@
+package net.pestarzt.pestarzt_discs.block.custom;
+
+import net.minecraft.world.level.block.Block;
+
+public class workbench extends Block {
+    public workbench(Properties properties) {
+        super(properties);
+    }
+
+}
--- src/main/java/net/pestarzt/pestarzt_discs/item/ModItems.java
+++ src/main/java/net/pestarzt/pestarzt_discs/item/ModItems.java
@@ -6,15 +6,17 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.pestarzt.pestarzt_discs.PestArzt_Discs;
+import net.pestarzt.pestarzt_discs.item.custom.badgay;
+import net.pestarzt.pestarzt_discs.item.custom.seaofcum;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, PestArzt_Discs.MOD_ID);
 
     public static final RegistryObject<Item> BadGay = ITEMS.register("badgay",
-            ()->new Item(new Item.Properties().stacksTo(1).tab(ModCreativeModeTab.PESTARZT_TAB)));
+            ()->new badgay(new Item.Properties().stacksTo(1).tab(ModCreativeModeTab.PESTARZT_TAB)));
     public static final RegistryObject<Item> SeaOfCum = ITEMS.register("seaofcum",
-            ()->new Item(new Item.Properties().stacksTo(1).tab(ModCreativeModeTab.PESTARZT_TAB)));
+            ()->new seaofcum(new Item.Properties().stacksTo(1).tab(ModCreativeModeTab.PESTARZT_TAB)));
     public static final RegistryObject<Item> DiscHolder = ITEMS.register("discholder",
             ()->new Item(new Item.Properties().tab(ModCreativeModeTab.PESTARZT_TAB)));
     public static void register(IEventBus eventBus){
